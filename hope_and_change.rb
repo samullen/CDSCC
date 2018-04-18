6a2d8374d00df456b18630e4ca6016e4ce54b30d
Samuel Mullen <samuel@samuelmullen.com>
2018-04-17 19:19:52 -0500

    Initial offering

---
+++ hope_and_change.rb
@@ -0,0 +1,23 @@
+def chanchanchange(change, payload = nil)
+  payload ||= { quarters: 0, dimes: 0, nickles: 0, pennies: 0 }
+
+  if change >= 0.25
+    difference = 0.25
+    payload[:quarters] = payload.fetch(:quarters, 0) + 1
+  elsif change >= 0.10
+    difference = 0.10
+    payload[:dimes] = payload.fetch(:dimes, 0) + 1
+  elsif change >= 0.05
+    difference = 0.05
+    payload[:nickles] = payload.fetch(:nickles, 0) + 1
+  elsif change >= 0.01
+    difference = 0.01
+    payload[:pennies] = payload.fetch(:pennies, 0) + 1
+  else
+    return payload
+  end
+
+  return chanchanchange((change - difference).round(2), payload)
+end
+
+puts chanchanchange(ARGV[0].to_f).map {|k,v| "#{v} #{k}"}.join(", ")
---
+++ hope_and_change_test.rb
@@ -0,0 +1,24 @@
+require "minitest/autorun"
+load "hope_and_change.rb"
+
+describe "hope and change" do
+  describe "#chanchanchange" do
+    it "returns a hash of coin counts based on the provided change value" do
+      results = chanchanchange(0.56)
+      results[:quarters].must_equal 2
+      results[:dimes].must_equal 0
+      results[:nickles].must_equal 1
+      results[:pennies].must_equal 1
+    end
+
+    it "accepts an optional payload" do
+      payload ||= { quarters: 10, dimes: 10, nickles: 10, pennies: 10 }
+      results = chanchanchange(0.87, payload)
+      results[:quarters].must_equal 13
+      results[:dimes].must_equal 11
+      results[:nickles].must_equal 10
+      results[:pennies].must_equal 12
+    end
+
+  end
+end
