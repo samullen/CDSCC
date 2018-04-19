c3c6c014fadf0177666482d9ebc16aa10fa5efad
Samuel Mullen <samuel@samuelmullen.com>
2018-04-18 21:13:27 -0500

    nix the need for defining test

--- challenge3.rb
+++ challenge3.rb
@@ -1,13 +1,11 @@
 require "./challenge_methods"
 
-if ENV["RUBY_ENV"] != "test"
-  switch_count = ARGF.gets.to_i
-  pairs = []
-  ARGF.each do |pair|
-    pairs << pair.split.map(&:to_i)
-  end
-
-  switches = toggle(switch_count, pairs)
-
-  puts switches.inject(:+)
+switch_count = ARGF.gets.to_i
+pairs = []
+ARGF.each do |pair|
+  pairs << pair.split.map(&:to_i)
 end
+
+switches = toggle(switch_count, pairs)
+
+puts switches.inject(:+)
